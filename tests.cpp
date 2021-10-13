5b74fdf19497987765c167100969424212167396
sidrathabib <sidrathabib@gmail.com>
2021-10-13 02:23:55 -0400

    another fix

--- tests.cpp
+++ tests.cpp
@@ -23,13 +23,13 @@ TEST_CASE("Task C: Vigenere") {
   CHECK(encryptVigenere("gogogo", "cat") == "iozqgh");
 }
 
-TEST_CASE("Task D Caesar Decryption") {
+TEST_CASE("Task D: Caesar") {
 	CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
   CHECK(decryptCaesar("A3zovm_q1kD zB@5xo...", 23) == "D3cryp_t1nG cE@5ar...");
   CHECK(decryptCaesar("F Qnlmy-Djfw Fufwy", 5) == "A Light-Year Apart");
 }
 
-TEST_CASE("Task D Vigenere Decryption") {
+TEST_CASE("Task D: Vigenere") {
 	CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
   CHECK(decryptVigenere("Fohlnrwg", "coding") == "Daedalus");
 	CHECK(decryptVigenere("Uiwyjrk yfwxnok", "beef") == "Testing testing");
